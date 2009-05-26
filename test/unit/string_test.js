f981d2f0748f77c7352082f9453b6a89e2a44804
Juriy Zaytsev <juriy@kangax.local>
2009-05-26 16:36:52 -0400

    Remove profile directives.

--- test/unit/string_test.js
+++ test/unit/string_test.js
@@ -1,6 +1,5 @@
 new Test.Unit.Runner({
   testInterpret: function(){
-    console.profile(1);
     this.assertIdentical('true', String.interpret(true));
     this.assertIdentical('123',  String.interpret(123));
     this.assertIdentical('foo bar', String.interpret('foo bar'));
@@ -558,6 +557,5 @@ new Test.Unit.Runner({
     this.assertIdentical(true, 'true'.evalJSON());
     this.assertIdentical(false, 'false'.evalJSON());
     this.assertEqual('"', '"\\""'.evalJSON());
-    console.profileEnd(1);
   }
 });
